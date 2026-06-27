16353f224658d9585489c3b7ccf643635f3ce6eb
aprameyadas3344 <gojosatoru.aprameyadas@gmail.com>
2026-06-27 10:58:47 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,10 +1,35 @@
+
 import turtle
-turtle.Screen().bgcolor("blue")
-sc = turtle.Screen()
-sc.setup(400, 400)
-turtle.title("welcome to turtle window")
-board = turtle.Turtle()
-for i in range (4):
-    board.forward(100)
-    board.left(90)
-    i = i+1
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("Colour Loop Artwork")
+artist = turtle.Turtle()
+artist.speed("fastest")
+artist.hideturtle()
+artist.pensize(2)
+
+def draw_petal(size, colour):
+    artist.color(colour)
+    artist.begin_fill()
+
+    for _ in range(2):
+        artist.circle(size, 60)
+        artist.left(120)
+
+    artist.end_fill()
+
+
+colours = ["red", "orange", "yellow", "lime", "cyan", "blue", "magenta"]
+
+for i in range(36):
+    draw_petal(90, colours[i % len(colours)])
+    artist.right(10)
+artist.penup()
+artist.goto(0, -25)
+artist.pendown()
+
+artist.color("white")
+artist.begin_fill()
+artist.circle(25)
+artist.end_fill()
+turtle.done()
